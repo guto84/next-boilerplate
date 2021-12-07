c6287db31eb6c74a22ef85834dfdeb2951930018
guto <gutob84@gmail.com>
2021-12-07 16:47:33 -0300

    feat: finalização boilerplate de base

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Next - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="manifest.json" />
+        <meta name="description" content="descrição" />
+      </Head>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </>
+  )
 }
 
-export default MyApp
+export default App
